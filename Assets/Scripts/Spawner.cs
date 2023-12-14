e52a8f341d56c878476b315d4a0881b1278f2cf3
Batuhan Şen <59296529+batuhansenn@users.noreply.github.com>
2023-12-14 06:13:50 +0300

    Add files via upload

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public float spawmRate = 1f;
+    public float minHeight = -1f;
+    public float maxHeight = 1f;
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), spawmRate, spawmRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn()
+    {
+        GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+    }
+}
